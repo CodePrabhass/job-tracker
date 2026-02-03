3210e6b31ebd3cccd5b06c23390afad41d2eea9a
Prabhas Goud <prabhasexample@gmail.com>
2026-02-03 17:46:46 +0530

    Day2: added Delete job feature with unique IDs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
 import './App.css'
+import {v4 as uuidv4} from 'uuid';
 
 /**
  * A React component that creates a job tracker application
  * Allows users to add and display job titles
  */
 function App() {
-  // State for the current job input
-  const [job,setJob] = useState("");
-  // State for the list of jobs
-  const [jobs,setJobsList] = useState([]);
+
+  const [job,setJob] = useState(""); 
+  const [jobsList,setJobsList] = useState([]);
+
+  const addJob = () => {
+    if(job.trim() !== ""){
+      const newJob={uniqueId:uuidv4(),title:job}
+      setJobsList([...jobsList,newJob]);
+      setJob("")
+    }  
+  }
+  const deleteJob = (uniqueId) => {
+    const updatedJobs = jobsList.filter((job)=>job.uniqueId!==uniqueId);
+    setJobsList(updatedJobs);
+  }
   return (
     <div className="App">
       <h1>JOB TRACKER App</h1>
@@ -18,20 +30,19 @@ function App() {
       value={job}
       onChange={(event)=>setJob(event.target.value)}
       />
-      <button onClick={()=>{
-        if(job.trim() !== ""){
-        setJobsList([...jobs,job]);
-        setJob("")
-      }}
-    }
-      >
+      <button onClick={addJob}>
         Add job
       </button>
+      
       <ul>
-        {jobs.map((item,index)=>(
-          <li key={index}>{item}</li>
+        {jobsList.map((each)=>(
+         <li key={each.uniqueId}>
+            {each.title} 
+          <button onClick={()=>deleteJob(each.uniqueId)}>DELETE</button>
+          </li>
         ))}
       </ul>
+      
     </div>
   );
 }
